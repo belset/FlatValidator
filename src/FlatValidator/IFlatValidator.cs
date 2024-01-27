701cebc47af6a6654e772e1afd15c9bfcdeca539
belset <belset@gmail.com>
2024-01-27 12:23:01 +0300

    XML comments fixed

--- src/FlatValidator/IFlatValidator.cs
+++ src/FlatValidator/IFlatValidator.cs
@@ -9,14 +9,14 @@ public interface IFlatValidator<TModel>
     /// <summary>
     /// Validates the specified instance.
     /// </summary>
-    /// <param name="instance">The instance to validate</param>
+    /// <param name="model">The instance to validate</param>
     /// <returns>A ValidationResult object containing any validation failures.</returns>
     FlatValidationResult Validate(in TModel model);
 
     /// <summary>
     /// Validates the specified instance.
     /// </summary>
-    /// <param name="instance">The instance to validate</param>
+    /// <param name="model">The instance to validate</param>
     /// <param name="timeout">A <see cref="TimeSpan"/> that represents the number of milliseconds
     /// to wait, or a <see cref="TimeSpan"/> that represents -1 milliseconds to wait indefinitely.
     /// </param>
@@ -26,7 +26,7 @@ public interface IFlatValidator<TModel>
     /// <summary>
     /// Validate the specified instance asynchronously
     /// </summary>
-    /// <param name="instance">The instance to validate</param>
+    /// <param name="model">The instance to validate</param>
     /// <param name="cancellation">The <see cref="CancellationToken"/> token to observe.</param>
     /// <returns>A ValidationResult object containing any validation failures.</returns>
     ValueTask<FlatValidationResult> ValidateAsync(TModel model, CancellationToken cancellation = default);
@@ -34,7 +34,7 @@ public interface IFlatValidator<TModel>
     /// <summary>
     /// Validate the specified instance asynchronously
     /// </summary>
-    /// <param name="instance">The instance to validate</param>
+    /// <param name="model">The instance to validate</param>
     /// <param name="timeout">A <see cref="TimeSpan"/> that represents the number of milliseconds
     /// to wait, or a <see cref="TimeSpan"/> that represents -1 milliseconds to wait indefinitely.
     /// </param>
