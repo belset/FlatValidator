268c3715b85125ae736189bb91f57576bbf13f6a
belset <belset@gmail.com>
2024-01-27 12:19:43 +0300

    Added method summaries

--- src/FlatValidator.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/FlatValidator.DependencyInjection/ServiceCollectionExtensions.cs
@@ -4,8 +4,18 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace System.Validation;
 
+/// <summary>
+/// Extensions to register classes in IServiceCollection
+/// </summary>
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Register all classes which are inherited of the FlatValidator from the assemblies in the IServiceCollection.
+    /// </summary>
+    /// <param name="services">IServiceCollection in which registration will be created.</param>
+    /// <param name="assemblies">List of assemblies which will be examined to find inheritances of the FlatValidator.</param>
+    /// <param name="lifetime">Specifies which kind of instance type will be registered.</param>
+    /// <returns>The same IServiceCollection</returns>
     public static IServiceCollection AddFlatValidatorsFromAssemblies(
                                         this IServiceCollection services,
                                         IEnumerable<Assembly> assemblies,
@@ -28,6 +38,13 @@ public static class ServiceCollectionExtensions
         return services;
     }
 
+    /// <summary>
+    /// Register all classes which are inherited of the FlatValidator from the assembliy in the IServiceCollection.
+    /// </summary>
+    /// <param name="services">IServiceCollection in which registration will be created.</param>
+    /// <param name="assembly">Assembly which will be examined to find inheritances of the FlatValidator.</param>
+    /// <param name="lifetime">Specifies which kind of instance type will be registered.</param>
+    /// <returns>The same IServiceCollection</returns>
     public static IServiceCollection AddFlatValidatorsFromAssembly(
                                         this IServiceCollection services,
                                         Assembly assembly,
