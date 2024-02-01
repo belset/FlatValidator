735912e3c7be47facaee5c4b1d1bcaaa7b5c963f
belset <belset@gmail.com>
2024-02-01 15:05:16 +0300

    Extract CapacityCache into independent file

--- src/FlatValidator/RuleList.cs
+++ src/FlatValidator/RuleList.cs
@@ -2,6 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+
 namespace System.Validation;
 
 internal enum RuleType : int
@@ -37,29 +38,6 @@ public struct Rule
     internal Expression MemberSelector1;
     internal Expression MemberSelector2;
     internal Expression MemberSelector3;
-}
-
-internal class CapacityCache
-{
-    internal const int DefaultCapacity = 8;
-    static int[] capacities = new int[256];
-
-    static CapacityCache()
-    {
-        Array.Fill(capacities, DefaultCapacity);
-    }
-
-    internal static int Get<TModel>()
-    {
-        return Volatile.Read(ref capacities[typeof(TModel).GetHashCode() & 0xFF]);
-    }
-    internal static void Set<TModel>(int value)
-    {
-        var index = typeof(TModel).GetHashCode() & 0xFF;
-        var capacity = Volatile.Read(ref capacities[index]);
-        capacity = ((capacity + capacity + capacity + value) >> 2) + 1;
-        Volatile.Write(ref capacities[index], capacity);
-    }
 }
 
 internal struct RuleList<TModel>
