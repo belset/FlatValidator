e7efc2150b1b6d812854b8b64095444447e15bcb
belset <belset@gmail.com>
2024-05-08 09:02:12 +0300

    PasswordStrengthClaculator+ShannonEntropy project added

---
+++ samples/PasswordStrengthClaculator+ShannonEntropy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Validation;
+
+bool abortProcess = false;
+Console.CancelKeyPress += (object? sender, ConsoleCancelEventArgs e) => e.Cancel = abortProcess = true;
+
+while (!abortProcess)
+{
+    Console.Write("Enter password:");
+    var password = Console.ReadLine();
+    if (string.IsNullOrEmpty(password)) 
+        break;
+
+    var passwordStrength = FlatValidatorFuncs.GetPasswordStrength(password!, out var score, out var maxScore);
+
+    Console.WriteLine($"   strength: {passwordStrength}, " +
+                      $"score/maxScore: {score}/{maxScore} = {Math.Round((double)score / maxScore, 2)}, " +
+                      $"entropy: {Math.Round(password.Length * FlatValidatorFuncs.GetShannonEntropy(password!))} bits " +
+                      $"({FlatValidatorFuncs.GetShannonEntropy(password!)})");
+}
